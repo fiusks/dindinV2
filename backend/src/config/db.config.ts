c34b172002cee05d6dea80737d291b74ec06a554
fiusks <rafabarros1@hotmail.com>
2022-06-16 23:37:58 -0300

    changed connections to db.config

---
+++ backend/src/config/db.config.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv'
+import {Knex,knex} from 'knex'
+dotenv.config()
+
+const config:Knex.Config={
+  client: "pg",
+  version: "^8.7.3",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+      rejectUnauthorized: false
+    },
+  },
+ 
+}
+const knexInstance = knex(config)
+
+export default knexInstance
