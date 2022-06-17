c1def25d0509096e501e06d70f7b7c995c327fbc
fiusks <rafabarros1@hotmail.com>
2022-06-16 23:37:31 -0300

    updated schema

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,18 +1,21 @@
 create Type transactionType as enum ('credit','debit');
 
-create table if not exists transactions (
-  id serial primary key,
-  date date not null,
-  description text,
-  amount bigint not null,
-  category text not null,
-  type transactionType not null
-  );
+
 
   create table if not exists users (
   id serial primary key,
   firstname text not null,
   lastname text not null,
-  email text not null,
+  email text not null unique,
   password text not null,
+  );
+
+  create table if not exists transactions (
+  id serial primary key,
+  user_id int references users(id)
+  date date not null,
+  description text,
+  amount bigint not null,
+  category text not null,
+  type transactionType not null
   );
