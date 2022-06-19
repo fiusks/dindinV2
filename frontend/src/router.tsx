8532394e842ad89d6d304fdb70a2c59087ae8359
Rafael <rafabarros1@hotmail.com>
2022-06-19 19:28:30 -0300

    signIn and signUp routes

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/layout';
-import Login from './components/login';
-
+import SignIn from './components/signIn';
+import SignUp from './components/signUp';
 
 export default function Router() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Navigate to="login" />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/" element={<Navigate to="signUp" />} />
+        <Route path="signUp" element={<SignUp />} />
+        <Route path="signIn" element={<SignIn />} />
       </Route>
     </Routes>
   );
