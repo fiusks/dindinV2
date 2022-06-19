b715a1abf2fabbe425da4a28d127767e25fa2677
Rafael <rafabarros1@hotmail.com>
2022-06-19 19:29:01 -0300

    signIn and signUp components

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,27 +1,32 @@
 import './styles.scss';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 import logo from '../../assets/images/logo.svg';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
-export default function Layout(){
-    return(
-        <Container fluid className="layout-container">
-        <Row className="layout-header">
-          <Col>
-            <div className="header-container">
-              <img src={logo} alt="App logo" />
-              <h1>Dindin</h1>
-            </div>
-          </Col>
-          <Col>
-          
-          </Col>
-        </Row>
-        <Row className="w-100 layout-body">
-          <Col>
-            <Outlet />
-          </Col>
-        </Row>
-      </Container>
-    )
+export default function Layout() {
+  return (
+    <Container fluid className="layout-container w-100">
+      <Navbar bg="light" className="layout-header w-100">
+        <Container fluid className="navbar-content">
+          <Navbar.Brand href="#home">
+            <img alt="app logo" src={logo} />
+            React Bootstrap
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="signIn">
+              Sign In
+            </Nav.Link>
+            <Nav.Link as={Link} to="signUp">
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Row className="w-100 layout-body">
+        <Col>
+          <Outlet />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
