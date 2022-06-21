825aae8fb06293a0f9743c4d24bce9c6550bd8be
fiusks <rafabarros1@hotmail.com>
2022-06-21 16:27:57 -0300

    added user reducer

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/User/userSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    user: userReducer,
   },
 });
 
