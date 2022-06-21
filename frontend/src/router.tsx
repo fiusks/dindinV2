7afb52408f1cf15e1e865afd08d3899b1e5bafba
fiusks <rafabarros1@hotmail.com>
2022-06-21 16:26:09 -0300

    private routing and home page creation

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,15 +1,27 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout';
+import TransactionList from './features/Transactions/TransactionList';
 import SignIn from './features/User/signIn';
 import SignUp from './features/User/signUp';
+import PrivateRoute from './helpers/PrivateRoute';
+import Home from './features/Home';
 
 export default function Router() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Navigate to="signUp" />} />
+        <Route path="/" element={<Home />} />
         <Route path="signUp" element={<SignUp />} />
         <Route path="signIn" element={<SignIn />} />
+        <Route
+          path="transactions"
+          element={
+            <PrivateRoute
+              redirectPath="/signIn"
+              children={<TransactionList />}
+            />
+          }
+        />
       </Route>
     </Routes>
   );
