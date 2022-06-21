208fc8e73af03b2ddd9244c5717902810851d91e
fiusks <rafabarros1@hotmail.com>
2022-06-21 17:37:50 -0300

    text added to home

--- frontend/src/features/Home/index.tsx
+++ frontend/src/features/Home/index.tsx
@@ -1,7 +1,11 @@
+import './styles.scss';
+import { Row, Col } from 'react-bootstrap';
 export default function Home() {
   return (
-    <>
-      <div>Home Page</div>
-    </>
+    <Row className="h-100">
+      <Col className="home-text">
+        <h1>Controle as suas finanças em um só lugar!</h1>
+      </Col>
+    </Row>
   );
 }
