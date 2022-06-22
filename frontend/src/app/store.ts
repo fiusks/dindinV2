57cfd2114b4839d647c0f5ddafdade096df3f4b7
fiusks <rafabarros1@hotmail.com>
2022-06-22 15:32:13 -0300

    add transactions reducer

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import userReducer from '../features/User/userSlice';
+import transactionsReducer from '../features/Transactions/transactionsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: userReducer,
+    transactions: transactionsReducer,
   },
 });
 
