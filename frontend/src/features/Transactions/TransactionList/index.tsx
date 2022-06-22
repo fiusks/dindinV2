c34fb02ae610401ea0641604db203eb1480819d7
fiusks <rafabarros1@hotmail.com>
2022-06-22 15:33:48 -0300

    Transactions table list

--- frontend/src/features/Transactions/TransactionList/index.tsx
+++ frontend/src/features/Transactions/TransactionList/index.tsx
@@ -1,7 +1,48 @@
+import './styles.scss';
+import { useAppSelector, useAppDispatch } from '../../../app/hooks';
+import { selectTransactions, transactionsList } from '../transactionsSlice';
+import { useEffect } from 'react';
+import { Table } from 'react-bootstrap';
+import { TransactionDocument } from '../../../types/transactions';
+import { dateFormat, weekdayFormat } from '../../../helpers/stringFormat';
+
 export default function TransactionList() {
+  const dispatch = useAppDispatch();
+  const transactions = useAppSelector(selectTransactions);
+
+  useEffect(() => {
+    dispatch(transactionsList());
+  }, []);
   return (
-    <>
-      <div>Lista de Transações</div>
-    </>
+    <Table hover borderless responsive>
+      <thead>
+        <tr>
+          <th className="tHide">Data</th>
+          <th className="tHide">Dia da Semana</th>
+          <th>Descrição</th>
+          <th className="tHide">Categoria</th>
+          <th>Valor</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.data.map((transaction: TransactionDocument) => (
+          <tr key={transaction.id}>
+            <td className="table-date tHide">{dateFormat(transaction.date)}</td>
+            <td className="tHide">{weekdayFormat(transaction.date)}</td>
+            <td>{transaction.description}</td>
+            <td className="tHide">{transaction.category}</td>
+            <td className={`${transaction.type}`}>{`R$  ${Number(
+              transaction.amount
+            ).toFixed(2)}`}</td>
+            <td className="table-action-buttons">
+              {/* <EditTransaction id={transaction.id!} />
+              <DeleteModal id={transaction.id!} /> */}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
