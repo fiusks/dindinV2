ba8ba32694b4ae57c1a7311d1daf6ab5f9b3c404
fiusks <rafabarros1@hotmail.com>
2022-06-22 15:34:23 -0300

    filter and transactions types

---
+++ frontend/src/types/transactions.ts
@@ -0,0 +1,26 @@
+import { IResponse } from './api';
+
+export interface TransactionDocument {
+  id: string;
+  date: string;
+  weekday: string;
+  description: string;
+  amount: string;
+  category: string;
+  type: 'credit' | 'debit';
+}
+export type transactionRegistration = Omit<
+  TransactionDocument,
+  'id' | 'weekday'
+>;
+export type ReponseTransactions = IResponse<TransactionDocument[]>;
+export interface FilterData {
+  filterValue: string;
+  isActive: boolean;
+}
+export interface TransactionFilters {
+  categories: FilterData[];
+  minValue: number;
+  maxValue: number;
+  weekday: FilterData[];
+}
