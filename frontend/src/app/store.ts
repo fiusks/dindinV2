d3b29016cab739ced7ff1f71cd97d4bba9610a17
Rafael <rafabarros1@hotmail.com>
2022-06-25 15:46:00 -0300

    add filter to store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import userReducer from '../features/User/userSlice';
 import transactionsReducer from '../features/Transactions/transactionsSlice';
+import filtersReducer from '../features/Filter/filtersSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: userReducer,
     transactions: transactionsReducer,
+    filters: filtersReducer,
   },
 });
 
