53c095a540b9f7ccdfa9d57f3f523154d5bbf844
Rafael <rafabarros1@hotmail.com>
2022-06-27 12:52:51 -0300

    update transaction list

--- frontend/src/features/Transactions/transactions.tsx
+++ frontend/src/features/Transactions/transactions.tsx
@@ -3,8 +3,22 @@ import { Row, Col } from 'react-bootstrap';
 import TransactionList from './TransactionList';
 import ResumeTransactions from './ResumeTransactions';
 import Filter from '../Filter';
+import { useEffect } from 'react';
+import { useAppDispatch } from '../../app/hooks';
+import { transactionsList } from './transactionsSlice';
+import { updateCategories } from '../Filter/filtersSlice';
+import { ReponseTransactions } from '../../types/transactions';
 
 export default function Transactions() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(transactionsList()).then((res) => {
+      const { data: transactions } = res.payload as ReponseTransactions;
+      dispatch(updateCategories(transactions));
+    });
+  }, []);
+
   return (
     <>
       <Row className="home-container">
