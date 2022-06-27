c8e5528189bb23ecde5399e9e5f3432ba06f37c1
fiusks <rafabarros1@hotmail.com>
2022-06-27 19:31:57 -0300

    change typing and add deleteTransaction reducer

--- frontend/src/features/Transactions/DeleteTransactionModal/deleteTransactionReducer.ts
+++ frontend/src/features/Transactions/DeleteTransactionModal/deleteTransactionReducer.ts
@@ -1,10 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ReponseTransactions } from '../../../types/transactions';
 import { getToken } from '../../../helpers/Auth/authHeader';
-import { transactionsList } from '../transactionsSlice';
+import { deleteTransaction } from '../transactionsSlice';
 const token = getToken();
 
-export const deleteTransactionById = createAsyncThunk<void, string>(
+export const deleteTransactionById = createAsyncThunk<void, number>(
   'transactions/deleteTransactionById',
   async (id, thunkAPI) => {
     const response = await fetch(`http://localhost:3001/transactions/${id}`, {
@@ -16,7 +16,7 @@ export const deleteTransactionById = createAsyncThunk<void, string>(
     });
     const { data } = (await response.json()) as ReponseTransactions;
     if (data) {
-      thunkAPI.dispatch(transactionsList());
+      thunkAPI.dispatch(deleteTransaction({ id }));
     }
   }
 );
