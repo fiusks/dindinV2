5bcd9f790354b9dd469c3e7030e7f10906559675
Rafael <rafabarros1@hotmail.com>
2022-06-27 12:53:59 -0300

    activeFilters types implementation

--- frontend/src/types/filter.ts
+++ frontend/src/types/filter.ts
@@ -1,17 +1,43 @@
 import { IResponse } from './api';
 
+type day =
+  | 'Segunda'
+  | 'Terça'
+  | 'Quarta'
+  | 'Quinta'
+  | 'Sexta'
+  | 'Sábado'
+  | 'Domingo';
+
 export interface IFiltersTransactions {
-  filterName: string;
-  value?: string;
-  state?: boolean;
+  [filterName: string]: boolean;
 }
-export interface IFiltersUpdate extends IFiltersTransactions {
-  filterType: 'weekday' | 'categories' | 'minValue' | 'maxValue';
+
+export interface IFilters {
+  weekday: string[];
+  categories: string[];
+  minValue: string;
+  maxValue: string;
 }
+export interface IActiveFilters {
+  weekday: false | string[];
+  categories: false | string[];
+  minValue: string;
+  maxValue: string;
+}
+// export interface IFiltersUpdate extends IFiltersTransactions {
+//   filterType: 'weekday' | 'categories' | 'minValue' | 'maxValue';
+// }
+export type teste = {
+  weekday: { [k: string]: string }[];
+  categories: { [k: string]: string }[];
+  minValue: string;
+  maxValue: string;
+};
 export interface IFilterOptions {
-  weekday: IFiltersTransactions[];
-  categories: IFiltersTransactions[];
-  minValue: IFiltersTransactions[];
-  maxValue: IFiltersTransactions[];
+  weekday: IFiltersTransactions;
+  categories: IFiltersTransactions;
+  minValue: string;
+  maxValue: string;
 }
 export type ResponseFilters = IResponse<IFilterOptions>;
