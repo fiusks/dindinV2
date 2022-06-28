4d0bc66e117b72404e3cd0199852223e40eec2c2
fiusks <rafabarros1@hotmail.com>
2022-06-28 00:39:13 -0300

    filter types correction

--- frontend/src/types/filter.ts
+++ frontend/src/types/filter.ts
@@ -1,6 +1,6 @@
 import { IResponse } from './api';
 
-type day =
+export type days =
   | 'Segunda'
   | 'Terça'
   | 'Quarta'
@@ -9,35 +9,16 @@ type day =
   | 'Sábado'
   | 'Domingo';
 
-export interface IFiltersTransactions {
-  [filterName: string]: boolean;
+export interface IFilterOptions {
+  categories: string[];
+  activeFilters: IActiveFilters;
 }
 
-export interface IFilters {
-  weekday: string[];
-  categories: string[];
-  minValue: string;
-  maxValue: string;
-}
 export interface IActiveFilters {
-  weekday: false | string[];
-  categories: false | string[];
-  minValue: string;
-  maxValue: string;
+  weekday?: string[];
+  categories?: string[];
+  minValue?: string;
+  maxValue?: string;
 }
-// export interface IFiltersUpdate extends IFiltersTransactions {
-//   filterType: 'weekday' | 'categories' | 'minValue' | 'maxValue';
-// }
-export type teste = {
-  weekday: { [k: string]: string }[];
-  categories: { [k: string]: string }[];
-  minValue: string;
-  maxValue: string;
-};
-export interface IFilterOptions {
-  weekday: IFiltersTransactions;
-  categories: IFiltersTransactions;
-  minValue: string;
-  maxValue: string;
-}
+
 export type ResponseFilters = IResponse<IFilterOptions>;
