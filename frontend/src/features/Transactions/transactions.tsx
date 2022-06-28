981f81ee046b03f256d4dbe2b10d24b04d5ac628
fiusks <rafabarros1@hotmail.com>
2022-06-28 00:41:17 -0300

    implement render list and filtered list

--- frontend/src/features/Transactions/transactions.tsx
+++ frontend/src/features/Transactions/transactions.tsx
@@ -4,20 +4,24 @@ import TransactionList from './TransactionList';
 import ResumeTransactions from './ResumeTransactions';
 import Filter from '../Filter';
 import { useEffect } from 'react';
-import { useAppDispatch } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { transactionsList } from './transactionsSlice';
-import { updateCategories } from '../Filter/filtersSlice';
-import { ReponseTransactions } from '../../types/transactions';
+import {
+  listFilteredTransactions,
+  selectFilters,
+} from '../Filter/filtersSlice';
 
 export default function Transactions() {
   const dispatch = useAppDispatch();
+  const filters = useAppSelector(selectFilters);
 
   useEffect(() => {
-    dispatch(transactionsList()).then((res) => {
-      const { data: transactions } = res.payload as ReponseTransactions;
-      dispatch(updateCategories(transactions));
-    });
-  }, []);
+    if (!Object.keys(filters.activeFilters).length) {
+      dispatch(transactionsList());
+    } else {
+      dispatch(listFilteredTransactions());
+    }
+  }, [filters.activeFilters]);
 
   return (
     <>
