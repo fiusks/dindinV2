bc5e2884905e47dc8c946c1d201ab730678b5988
fiusks <rafabarros1@hotmail.com>
2022-06-29 00:48:54 -0300

    schema correction

--- backend/schema.sql
+++ backend/schema.sql
@@ -7,12 +7,12 @@ create Type transactionType as enum ('credit','debit');
   firstname text not null,
   lastname text not null,
   email text not null unique,
-  password text not null,
+  password text not null
   );
 
   create table if not exists transactions (
   id serial primary key,
-  user_id int references users(id)
+  user_id int references users(id),
   date date not null,
   description text,
   amount bigint not null,
