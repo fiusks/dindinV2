d8874ea6109c127a4c3bf880b47c0bf2f5458630
fiusks <rafabarros1@hotmail.com>
2022-06-29 02:33:09 -0300

    map bug when no data to show

--- frontend/src/features/Transactions/TransactionList/index.tsx
+++ frontend/src/features/Transactions/TransactionList/index.tsx
@@ -11,35 +11,39 @@ export default function TransactionList() {
   const transactions = useAppSelector(selectTransactions);
 
   return (
-    <Table hover borderless>
-      <thead>
-        <tr>
-          <th className="tHide">Data</th>
-          <th className="tHide">Dia da Semana</th>
-          <th>Descrição</th>
-          <th className="tHide">Categoria</th>
-          <th>Valor</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.data.map((transaction: TransactionDocument) => (
-          <tr key={transaction.id}>
-            <td className="table-date tHide">{dateFormat(transaction.date)}</td>
-            <td className="tHide">{weekdayFormat(transaction.date)}</td>
-            <td>{transaction.description}</td>
-            <td className="tHide">{transaction.category}</td>
-            <td className={`${transaction.type}`}>{`R$  ${Number(
-              transaction.amount
-            ).toFixed(2)}`}</td>
-            <td className="table-action-buttons">
-              <EditTransaction id={transaction.id} />
-              <DeleteModal id={transaction.id} />
-            </td>
+    <>
+      <Table hover borderless>
+        <thead>
+          <tr>
+            <th className="tHide">Data</th>
+            <th className="tHide">Dia da Semana</th>
+            <th>Descrição</th>
+            <th className="tHide">Categoria</th>
+            <th>Valor</th>
+            <th></th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {transactions.data?.map((transaction: TransactionDocument) => (
+            <tr key={transaction.id}>
+              <td className="table-date tHide">
+                {dateFormat(transaction.date)}
+              </td>
+              <td className="tHide">{weekdayFormat(transaction.date)}</td>
+              <td>{transaction.description}</td>
+              <td className="tHide">{transaction.category}</td>
+              <td className={`${transaction.type}`}>{`R$  ${Number(
+                transaction.amount
+              ).toFixed(2)}`}</td>
+              <td className="table-action-buttons">
+                <EditTransaction id={transaction.id} />
+                <DeleteModal id={transaction.id} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 }
